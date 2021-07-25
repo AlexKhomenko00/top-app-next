23e9aee74c9c77d81630f48e1ebc601a53ff4bc1
Alex Khomenko <sasvsha@gmail.com>
2021-07-26 00:44:02 +0200

    enhanced sidebar markup

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,11 +1,22 @@
 import { DetailedHTMLProps, HTMLAttributes } from "react";
+import cn from "classnames";
+
 import Menu from "../Menu/Menu";
 
-const Sidebar = (
-  props: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
-): JSX.Element => {
+import Logo from "../Logo.svg";
+
+import s from "./Sidebar.module.css";
+
+const Sidebar = ({
+  className,
+  ...props
+}: DetailedHTMLProps<
+  HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>): JSX.Element => {
   return (
-    <div {...props}>
+    <div className={cn(className, s.sidebar)} {...props}>
+      <Logo className={s.logo} />
       <Menu />
     </div>
   );
