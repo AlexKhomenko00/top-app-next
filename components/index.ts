6f696c839b3fa5f4719213af9f82ca4b90ee2955
Alex Khomenko <sasvsha@gmail.com>
2021-08-08 22:35:01 +0200

    added hhData import/export

--- components/index.ts
+++ components/index.ts
@@ -4,3 +4,4 @@ export { default as Paragraph } from "./Paragraph/Paragraph";
 export { default as Tag } from "./Tag/Tag";
 export { default as Rating } from "./Rating/Rating";
 export { default as Card } from "./Card/Card";
+export { default as HhData } from "./HhData/HhData";
